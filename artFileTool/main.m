2ce3981d1d112e17f5bd677250587f08e8dd903a
Alex Zielenski <Alex@Alexs-MacBook-Pro.local>
2011-06-10 13:13:45 -0400

    Initial Commit

---
+++ artFileTool/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  artFileTool
+//
+//  Created by Alex Zielenski on 6/10/11.
+//  Copyright 2011 Alex Zielenski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
